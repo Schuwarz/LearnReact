cc6d56b70f42d84d1dd56be156825da938dc989b
schuwarz <schuwarz@outlook.com>
2026-04-02 18:11:39 +0500

    Домашка по теме Рендер списков

--- src/components.jsx
+++ src/components.jsx
@@ -3,3 +3,40 @@ export default function Хуй(str) {
   return <div>{str}</div>
 }
 
+export default function List() {
+  const chemists = people.filter((person) => person.profession === "химик");
+
+  const others = people.filter((person) => person.profession !== "химик");
+
+  const listItems = chemists.concat(others).map((person) => (
+    <li key={person.id}>
+      <img src={getImageUrl(person)} alt={person.name} />
+      <p>
+        <b>{person.name}:</b>
+        {" " + person.profession}. Достижение: {person.accomplishment}
+      </p>
+    </li>
+  ));
+  return (
+    <article>
+      <h1>Ученые</h1>
+      <ul>{listItems}</ul>
+    </article>
+  );
+}
+
+export default function RecipeList() {
+  const itemList = recipes.map((item) => (
+    <ul>
+      <h2>{item.name}</h2>
+      <ul>{item.ingredients.map((item) => <li>{item}</li>)}</ul>
+    </ul>
+  ));
+
+  return (
+    <div>
+      <h1>Рецепты</h1>
+      {itemList}
+    </div>
+  );
+}
